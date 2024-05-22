5a6369c19aca32af74a5413dad0371f05bc7cbd3
xAhmedMoussax <xahmedmoussa@gmail.com>
2024-05-22 15:54:04 -0700

    hero updated

--- client/src/components/hero.tsx
+++ client/src/components/hero.tsx
@@ -1,11 +1,12 @@
-"use client"; 
+"use client";
 import React, { useState } from "react";
-import { Textarea } from "./ui/textarea";
 import { CheckCheck } from "lucide-react";
+import axios from "axios";
+import { Textarea } from "./ui/textarea";
 import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 import { Sedgwick_Ave_Display } from "next/font/google";
 import { cn } from "@/lib/utils";
-import axios from "axios";
 
 const font = Sedgwick_Ave_Display({
   subsets: ["latin"],
@@ -16,7 +17,7 @@ const Hero = () => {
   const [comment, setComment] = useState("");
   const [response, setResponse] = useState<string | null>(null);
 
-  const handleCommentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleCommentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setComment(e.target.value);
   };
 
@@ -67,48 +68,46 @@ const Hero = () => {
         </ul>
       </div>
       <div>
-        <div className="flex justify-end">
-          <Textarea
-            className="p-4 flex h-36 w-[32rem] resize-none"
+        <div className="flex flex-col space-y-4 p-3 rounded-xl bg-gray-900/5 ring-1 ring-inset ring-gray-900/10">  
+         <div className="flex items-center gap-x-3 w-full">
+          <Input
+            className="flex"
             placeholder="Enter Your Comment ..."
             value={comment}
             onChange={handleCommentChange}
           />
-        </div>
-        <div className="relative px-8 sm:px-16 md:px-0 md:mx-auto md:max-w-xl w-full lg:mx-0">
-          <img
-            alt="try-it"
-            aria-hidden="true"
-            src="/file.png"
-            className="absolute w-[6.5rem] left-2/3 -top-0 select-none hidden sm:block"
-          />
-          <div className="flex justify-end mt-2">
+          <div className="flex justify-end">
             <Button className="bg-red-600" onClick={handleCheckToxicity}>
               Check Toxicity
             </Button>
           </div>
-        </div>
-        {response && (
-          <div className="mt-4 p-4 bg-gray-100 rounded-lg">
-            <h2 className="text-2xl mb-4">Response:</h2>
-            <table className="min-w-full bg-white">
-              <thead>
-                <tr>
-                  <th className="py-2 px-4 border-b border-gray-200">Label</th>
-                  <th className="py-2 px-4 border-b border-gray-200">Value</th>
-                </tr>
-              </thead>
-              <tbody>
-                {parseResponse(response).map(({ label, value }) => (
-                  <tr key={label}>
-                    <td className="py-2 px-4 border-b border-gray-200">{label}</td>
-                    <td className="py-2 px-4 border-b border-gray-200">{value ? "True" : "False"}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
           </div>
-        )}
+          <div className="h-36 mt-4 rounded-lg border-2 border-dashed border-zinc-300 text-sm flex items-center justify-center">
+            {response ? (
+              <div className="w-full h-full overflow-y-auto p-4 ">
+                <h2 className="text-2xl mb-4">Response:</h2>
+                <table className="min-w-full bg-white">
+                  <thead>
+                    <tr>
+                      <th className="py-2 px-4 border-b border-gray-200">Label</th>
+                      <th className="py-2 px-4 border-b border-gray-200">Value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {parseResponse(response).map(({ label, value }) => (
+                      <tr key={label}>
+                        <td className="py-2 px-4 border-b border-gray-200">{label}</td>
+                        <td className="py-2 px-4 border-b border-gray-200">{value ? "True" : "False"}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            ) : (
+              "Results will be shown here"
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
