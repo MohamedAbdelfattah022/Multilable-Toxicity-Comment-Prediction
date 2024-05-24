092f5c242a34c6c8cd0c93e0ef4524e91105f46d
Ahmed Moussa <144591704+xAhmedMoussax@users.noreply.github.com>
2024-05-24 12:41:55 -0700

    Update navbar.tsx

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -18,11 +18,13 @@ const NavBar = () => {
             <Biohazard className='w-10 h-10 text-red-600 mr-3 mb-1.5' /> <p className={cn("flex text-3xl font-extrabold select-none", font.className)}>IsToxic</p>
         </div>
         <div className='gap-5 flex items-center'>
-            <Link href={''} className='font-semibold'>About</Link>
-            <Link href={''} className='font-semibold'>Privacy Policy</Link>
+            <Link href='/About' className='font-semibold hover:-translate-y-1 transition-all'>About</Link>
+            <Link href='/PrivacyPolicy' className='font-semibold hover:-translate-y-1 transition-all'>Privacy Policy</Link>
+            <Link href="https://github.com/MohamedAbdelfattah022/Multilable-Toxicity-Comment-Prediction/tree/master" target='_blank'>
             <Button className='px-6 bg-red-600 '>
                 GitHub Repo
             </Button>
+            </Link>
         </div>
     </div>
   )
